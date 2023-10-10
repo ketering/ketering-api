d6fe9c612b16be278eef9a45c7922516a03cc292
Petar Simonovic <simonovicp12@gmail.com>
2023-10-10 21:36:32 +0200

    added all our companies to db

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Company;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,11 +13,49 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        $companies = [
+            'Alicorn',
+            'Amplitudo',
+            'Bild Studio',
+            'Codeus',
+            'Coinis',
+            'DataDesign',
+            'Oykos Development',
+            'Logate',
+            'Fleka',
+            'Domain',
+            'Čikom',
+            'AI Solution',
+            'Pos4me',
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+            'Digital Bee',
+            'Code Pixel',
+            'Arhimed',
+            'Business Intelligence Consulting doo Podgorica',
+            'Omitech',
+            'Codelab',
+            'Sky Sat',
+            'Progmata',
+            'ECS',
+            '2BI',
+            'Optimus Consulting',
+            'Sky Express',
+            'Simes Inžinjering',
+            'Telemont',
+            'Studio Lasso',
+            'Profit app',
+            'ZZI',
+            '3d soba',
+            'Navira IT',
+
+            'Telekom',
+            'NTP',
+            'Tehnopolis'
+        ];
+        foreach ($companies as $company) {
+            Company::factory([
+                'name' => $company,
+            ])->create();
+        }
     }
 }
