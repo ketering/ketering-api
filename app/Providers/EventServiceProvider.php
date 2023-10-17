78e0c0cca6ec2e84f3ca924442d4519aa7e2767b
Petar Simonovic <simonovicp12@gmail.com>
2023-10-18 00:52:32 +0200

    darkmode data gets persisted to db

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -6,6 +6,8 @@ use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Event;
+use JeroenNoten\LaravelAdminLte\Events\DarkModeWasToggled;
+use JeroenNoten\LaravelAdminLte\Events\ReadingDarkModePreference;
 
 class EventServiceProvider extends ServiceProvider
 {
@@ -25,7 +27,17 @@ class EventServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Register listener for ReadingDarkModePreference event. We use this event to setup dark mode initial status for AdminLTE package.
+        Event::listen(
+            ReadingDarkModePreference::class,
+            [$this, 'handleReadingDarkModeEvt']
+        );
+
+        // Register listener for DarkModeWasToggled AdminLTE event.
+        Event::listen(
+            DarkModeWasToggled::class,
+            [$this, 'handleDarkModeWasToggledEvt']
+        );
     }
 
     /**
@@ -34,5 +46,38 @@ class EventServiceProvider extends ServiceProvider
     public function shouldDiscoverEvents(): bool
     {
         return false;
+    }
+
+    /**
+     * Handle the ReadingDarkModePreference AdminLTE event.
+     *
+     * @param ReadingDarkModePreference $event
+     * @return void
+     */
+    public function handleReadingDarkModeEvt(ReadingDarkModePreference $event): void
+    {
+        $darkModeCfg = \Auth::user()->dark_mode;
+
+        // Setup initial dark mode preference.
+        if ($darkModeCfg) {
+            $event->darkMode->enable();
+        } else {
+            $event->darkMode->disable();
+        }
+    }
+
+    /**
+     * Handle the DarkModeWasToggled AdminLTE event.
+     *
+     * @param DarkModeWasToggled $event
+     * @return void
+     */
+    public function handleDarkModeWasToggledEvt(DarkModeWasToggled $event): void
+    {
+        $darkModeCfg = $event->darkMode->isEnabled();
+
+        $user = \Auth::user();
+        $user->dark_mode = $darkModeCfg;
+        $user->save();
     }
 }
